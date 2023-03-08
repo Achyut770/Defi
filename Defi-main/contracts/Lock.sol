fd7f2914f8395e26b1afce37733594a771da4f2b
Achyut770 <104488153+Achyut770@users.noreply.github.com>
2023-03-08 12:34:28 +0545

    Update Lock.sol

--- Defi-main/contracts/Lock.sol
+++ Defi-main/contracts/Lock.sol
@@ -4,7 +4,6 @@ pragma solidity 0.8.17;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
-import "hardhat/console.sol";
 
 contract Defi is ReentrancyGuard {
     struct StreamDetails {
